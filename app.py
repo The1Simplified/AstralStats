2d629f6739b7713ff028dde447b8d7714c2fdc73
Simpl1f1ed <scheiterlem@gmail.com>
2022-06-11 18:35:33 -0400

    Added Production Functionality

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from waitress import serve
+
 from flask import Flask, render_template, redirect, url_for, session
 from flask_session import Session
 
@@ -33,4 +35,5 @@ async def steam_user_account():
     return render_template("steam.html")
 
 if __name__ == "__main__":
-    app.run(host="localhost", port=5000, debug=True)
+    # app.run(host="localhost", port=5000)  # Dev
+    serve(app, host="0.0.0.0", port=5000) # Production
