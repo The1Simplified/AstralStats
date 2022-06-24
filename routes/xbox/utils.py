3216ec2db7a1d309aeccba5576e8ce25a4d11d9c
Simpl1f1ed <scheiterlem@gmail.com>
2022-06-24 14:58:29 -0400

    Added dynamic os checking for asyncio event loop policy

--- routes/xbox/utils.py
+++ routes/xbox/utils.py
@@ -2,13 +2,15 @@ import asyncio
 import os
 import sys
 import aiohttp
+import platform
 
 from xbox import *
 from xbox.webapi.api.client import XboxLiveClient
 from xbox.webapi.authentication.manager import AuthenticationManager
 from xbox.webapi.authentication.models import OAuth2TokenResponse
 
-asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+if 'windows' in platform.system().lower():
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 client_id = os.environ.get("MS_CLIENT_ID")
 client_secret = os.environ.get("MS_CLIENT_SECRET")
@@ -48,12 +50,14 @@ async def get_user_info(xuid: str = ""):
         os.environ["PRIMARY_SIGN_IN_TOKEN"] = auth_mgr.oauth.json()
         xbl_client = XboxLiveClient(auth_mgr)
         friends_raw = await xbl_client.people.get_friends_by_xuid(xuid)
-        friends_invert_sorted = sorted(friends_raw.people, key=lambda d: d.presence_state)
-        
+        friends_invert_sorted = sorted(friends_raw.people,
+                                       key=lambda d: d.presence_state)
+
         user_friends_sorted = friends_invert_sorted[::-1]
         flag = False
         for i in friends_invert_sorted:
-            friends_of_friends = await xbl_client.people.get_friends_by_xuid(i.xuid)
+            friends_of_friends = await xbl_client.people.get_friends_by_xuid(
+                i.xuid)
             for x in friends_of_friends.people:
                 if x.xuid == xuid:
                     flag = True
@@ -61,5 +65,5 @@ async def get_user_info(xuid: str = ""):
                     break
             if flag:
                 break
-        
+
         return user_friends_sorted, user_data
