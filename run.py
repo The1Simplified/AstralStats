3f866c4ea7d206930786d50ae35f6f8e1146313b
Simpl1f1ed <scheiterlem@gmail.com>
2022-06-24 20:07:06 -0400

    Activating debug for dev

--- run.py
+++ run.py
@@ -4,4 +4,5 @@ from __init__ import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    serve(app, host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+    #serve(app, host="0.0.0.0", port=5000)
