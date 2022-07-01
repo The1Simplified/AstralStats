efc19027b195cd8e9ac90e03e9e00f0753e1dd61
Simpl1f1ed <scheiterlem@gmail.com>
2022-07-01 06:40:56 -0400

    Added DB env varibale and edited user table

--- config.py
+++ config.py
@@ -1,11 +1,12 @@
 import secrets
+import os
 
 
 class Config:
     SECRET_KEY = f'{secrets.token_hex(16)}'
     SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
         username="Simpl1f1ed",
-        password="test",
+        password=os.environ.get("DB_PASSOWRD"),
         hostname="Simpl1f1ed.mysql.pythonanywhere-services.com",
         databasename=
         "Simpl1f1ed$AstralStats_DB",
--- models.py
+++ models.py
@@ -13,9 +13,9 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     rank = db.Column(db.String, nullable=False, default='user')
     username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(60), nullable=False)
-    xbox_token = db.Column(db.String())
+    xbox_token = db.Column(db.String)
 
     def get_reset_token(self, expires_sec=1800):
         serialized = Serializer(current_app.config['SECRET_KEY'], expires_sec)
