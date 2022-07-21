88a6b553513b26061c56769cfbc0d322d42d85d6
Simpl1f1ed <49690288+The1Simplified@users.noreply.github.com>
2022-07-21 01:59:46 -0400

    Version 2 (#2)
    * Delete settings.json
    
    * Added registration functionality
    
    * Update LICENSE

--- routes/main/routes.py
+++ routes/main/routes.py
@@ -1,16 +1,67 @@
-from flask import Blueprint, render_template, request, session
+from flask import Blueprint, redirect, render_template, request, session, url_for, flash
+from flask_login import login_user, current_user, logout_user, login_required
+from routes.main.forms import LoginForm, RegistrationForm
 from routes.xbox.utils import get_user_search
+from __init__ import db, bcrypt
+from models import User
 
 main = Blueprint('main', __name__)
+
 
 @main.route("/", methods=["GET", "POST"])
 async def homepage():
     return render_template("homepage.html")
 
 
-@main.route("/search/<string:platform>/<string:username>", methods=["GET", "POST"])
+@main.route("/search/<string:platform>/<string:username>",
+            methods=["GET", "POST"])
 async def saerch(platform: str = "", username: str = ""):
     platform_string = f"color:var(--provider-color-{platform})"
     xbox_search = await get_user_search(username)
 
-    return render_template("search.html", platform=platform, username=username, xbox_search=xbox_search, platform_string=platform_string)
+    return render_template("search.html",
+                           platform=platform,
+                           username=username,
+                           xbox_search=xbox_search,
+                           platform_string=platform_string)
+
+
+@main.route("/login", methods=["GET", "POST"])
+async def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.homepage'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and bcrypt.check_password_hash(user.password,
+                                               form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(
+                url_for('main.home'))
+        else:
+            flash('Username and Password do not match', 'warning')
+    return render_template('login.html', title='Login', form=form)
+
+
+@main.route("/logout", methods=["GET", "POST"])
+async def logout():
+    logout_user()
+    return redirect(url_for('main.home'))
+
+
+@main.route("/register", methods=["GET", "POST"])
+async def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.homepage'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(
+            form.password.data).decode('utf-8')
+        user = User(username=form.username.data,
+                    email=form.email.data,
+                    password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('users.login'))
+    return render_template('register.html', title='Register', form=form)
--- routes/xbox/routes.py
+++ routes/xbox/routes.py
@@ -1,7 +1,9 @@
 from aiohttp import ClientResponseError
 from flask import Blueprint, redirect, render_template, request, flash
+from flask_login import current_user
 from pydantic import ValidationError
 from routes.xbox.utils import get_user_info
+from __init__ import db
 
 xbox = Blueprint('xbox', __name__)
 
@@ -13,8 +15,17 @@ async def main():
 
 @xbox.route("/xbox/xuid/<string:xuid>", methods=["GET", "POST"])
 async def xbox_user_account(xuid: str = ""):
+    
+    client_token = ''
+    #if current_user.is_authenticated and len(current_user.xbox_token) > 1:
+    #    client_token = current_user.xbox_token
+    
+    
     try:
-        user_friends_sorted, user_data = await get_user_info(xuid)
+        user_friends_sorted, user_data, token_refresh = await get_user_info(xuid, client_token)
+        if current_user.is_authenticated and token_refresh:
+            current_user.xbox_token = token_refresh
+            db.session.commit()
     except ClientResponseError:
         flash("Sorry this account cannont be accessed: Error 403", 'error')
         return redirect(request.referrer)
